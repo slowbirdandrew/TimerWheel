09cb24207c4d3062a76399df25145ff4bf63019c
slowbirdandrew <623791820@qq.com>
2020-12-17 23:09:21 +0800

    支持非类成员回调函数定时器的添加

--- timer_manager.cpp
+++ timer_manager.cpp
@@ -14,9 +14,21 @@ void CTimerManager::_UnInit()
     }
 }
 
-HTIMER CTimerManager::AddTimer(const CDelegate& delegate, CArgs* pDelegateArgs, void* pArgs, int32_t nInterval, int32_t nCount, const char* szFile /*= ""*/, int32_t uLine /*= 0*/)
+HTIMER CTimerManager::AddTimer(const CDelegate& delegate, CArgs* pDelegateArgs, void* pArgs, int32_t nInterval, int32_t nCount, const char* szFile /*= ""*/, int32_t nLine /*= 0*/)
 {
-    return m_pTimer->AddTimer(delegate, pDelegateArgs, pArgs, nInterval, nCount, szFile, uLine);
+    return m_pTimer->AddTimer(delegate, pDelegateArgs, pArgs, nInterval, nCount, szFile, nLine);
+}
+
+HTIMER CTimerManager::AddTimer(const std::function<bool(CArgs*, void*)>& callback, CArgs* pDelegateArgs, void* pArgs, int32_t nInterval, int32_t nCount, const char* szFile /*= ""*/, int32_t nLine /*= 0*/)
+{
+    if (!callback)
+    {
+        return INVALIDE_HTIMER;
+    }
+    auto* pDelegateMethod = new DelegateMethod;
+    pDelegateMethod->m_pFunc = callback;
+    pDelegateMethod->m_pData = pArgs;
+    return AddTimer(DELEGATE_COMBINATION(CTimerManager, ExecuteDelegateMethod, this), nullptr, reinterpret_cast<void*>(pDelegateMethod), nInterval, nCount, szFile, nLine);
 }
 
 void CTimerManager::KillTimer(HTIMER uTimer)
@@ -32,5 +44,17 @@ void CTimerManager::KillAllTimer()
 void CTimerManager::Update(int32_t nDelta)
 {
     m_pTimer->Update(nDelta);
+}
+
+bool CTimerManager::ExecuteDelegateMethod(CArgs* pArgs, void* pData)
+{
+    if (!pData)
+    {
+        return false;
+    }
+    auto* pDelegateMethod = reinterpret_cast<DelegateMethod*>(pData);
+    bool bRes = pDelegateMethod->m_pFunc(pArgs, pData);
+    delete pDelegateMethod;
+    return bRes;
 }
 
--- timer_manager.h
+++ timer_manager.h
@@ -1,6 +1,7 @@
 ﻿#ifndef __TIMER_MANAGER_H__
 #define __TIMER_MANAGER_H__
 
+#include <functional>
 #include "timer_wheel.h"
 
 class CTimerManager
@@ -25,20 +26,42 @@ private:
      * @return void
      */
     void _UnInit();
+    /**
+     * @brief 包装非类成员函数的结构体
+    */
+    struct DelegateMethod
+    {
+        // 定时任务回调函数
+        std::function<bool(CArgs*, void*)> m_pFunc;
+        // 定时任务数据
+        void* m_pData{ nullptr };
+    };
 
 public:
     /**
      * @brief 添加一个定时器任务
      * @param delegate 定时任务执行对象
-     * @param pDelegateArgs 定时任务参数
-     * @param pArgs 额外参数
+     * @param pDelegateArgs 定时任务执行对象参数
+     * @param pArgs 定时任务参数
      * @param nInterval 执行间隔
      * @param nCount 执行次数
      * @param szFile 定时器添加的文件
-     * @param uLine 定时器添加的代码行
+     * @param nLine 定时器添加的代码行
      * @return HTIMER 定时器句柄
      */
-    HTIMER AddTimer(const CDelegate& delegate, CArgs* pDelegateArgs, void* pArgs, int32_t nInterval, int32_t nCount, const char* szFile = "", int32_t uLine = 0);
+    HTIMER AddTimer(const CDelegate& delegate, CArgs* pDelegateArgs, void* pArgs, int32_t nInterval, int32_t nCount, const char* szFile = "", int32_t nLine = 0);
+    /**
+     * @brief 添加一个定时器任务
+     * @param callback 定时任务回调
+     * @param pDelegateArgs 定时任务执行对象参数
+     * @param pArgs 定时任务参数
+     * @param nInterval 执行间隔
+     * @param nCount 执行次数
+     * @param szFile 添加定时器的文件
+     * @param nLine 添加定时器的代码行数
+     * @return HTIMER 定时器句柄
+     */
+    HTIMER AddTimer(const std::function<bool(CArgs*, void*)>& callback, CArgs* pDelegateArgs, void* pArgs, int32_t nInterval, int32_t nCount, const char* szFile = "", int32_t nLine = 0);
     /**
      * @brief 删除一个定时器任务
      * @param uTimer 定时器句柄
@@ -56,6 +79,13 @@ public:
      * @return void
      */
     void Update(int32_t nDelta);
+    /**
+     * @brief 非类成员函数结构体的执行函数
+     * @param pArgs 定时任务对象参数
+     * @param pData 定时任务数据
+     * @return bool 返回值
+     */
+    bool ExecuteDelegateMethod(CArgs* pArgs, void* pData);
 
 private:
     TimerWheel* m_pTimer{ nullptr };
